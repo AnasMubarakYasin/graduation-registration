4d3a97c578a6ab0cf1149a4b02c2fedb01c5eebe
anasmubarakyasin <bladerlaiga.97@gmail.com>
2023-02-21 18:19:45 +0800

    fix: operator create

--- resources/js/admin/user/operator/edit.js
+++ resources/js/admin/user/operator/edit.js
@@ -1,14 +1,3 @@
-import {
-  input_img_preview
-} from "../../../lib";
-
-input_img_preview("photo", (url) => {
-  const img = document.getElementById('photo_preview');
-  img.src = url;
-  img.classList.remove('hidden')
-  document.getElementById('photo_placeholder').classList.add('hidden');
-});
-
 const department = document.getElementById('department');
 const faculty = document.getElementById('field_faculty');
 
